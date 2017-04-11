d1520a3477a01f4e0f410627083f8fd92c06995c
Ivan Panov <ivan.panov@mapp.com>
2017-04-11 17:42:11 +0200

    added hardcoded spots data; put markers on map from spots endpoint

--- services/SpotsService.js
+++ services/SpotsService.js
@@ -1,18 +1,37 @@
 var monk = require('monk')
 var config = require('../config')
 
+var getAllFromMongo = function() {
+    return new Promise((resolve, reject) => {
+                monk(config.mongoUrl)
+                    .then((db) => {
+                           db.get('spots').find({}, {sort: {name: 1}})
+                                .then(function (spots) {
+                                    resolve(spots)
+                                })
+                                .catch((err) => reject(err))
+                           })
+                           .catch((err) => {
+                                reject(err) })})}
+
+var spotsData = [{
+    title: 'Skinny bench',
+    description: 'Relatively easy skinny (broad, not high from the ground)',
+    position: {
+        lat: 48.132629, lng: 11.614861
+    }
+}]
+
+var getAllHardcoded = function() {
+    return new Promise((resolve, reject) => {
+        resolve(spotsData)
+    });
+}
+
 var SpotsService = {
     getAll: function() {
-        return new Promise((resolve, reject) => {
-            monk(config.mongoUrl)
-                .then((db) => {
-                       db.get('spots').find({}, {sort: {name: 1}})
-                            .then(function (spots) {
-                                resolve(spots)
-                            })
-                            .catch((err) => reject(err))
-                       })
-                       .catch((err) => {
-                            reject(err) })})}}
+        return getAllHardcoded() }}
+
+
 
 module.exports = SpotsService;
